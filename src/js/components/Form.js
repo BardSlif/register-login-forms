ce4dc1d40555b8870636b1190fb90ba663b0f869
Bartosz Ślifirski <b.slifirski@gmail.com>
2018-10-01 14:55:00 +0200

    Add Form component

---
+++ src/js/components/Form.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+
+    render() {
+        return (
+            <div className='form-wrapper'>
+                <form>
+
+                </form>
+            </div>
+        );
+    }
+
+};
+
+export default Form;

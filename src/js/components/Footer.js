489a51388e6dc7d7189363466ff30078b039a3b7
Bartosz Ślifirski <b.slifirski@gmail.com>
2018-10-06 12:51:41 +0200

    Add social link

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -7,6 +7,7 @@ class Footer extends Component {
                 <a className='social-link instagram' href='https://www.instagram.com'>Instagram</a>
                 <a className='social-link facebook' href='https://www.facebook.com'>Facebook</a>
                 <a className='social-link pinterest' href='https://www.pinterest.com'>Pinterest</a>
+                <a className='social-link logo' href='https://github.com/BardSlif'>BŚ</a>
             </div>
         )
     }

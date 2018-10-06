a63cbb465f2ae99d2d3a78786f51a8b97f3f3d2b
Bartosz Ślifirski <b.slifirski@gmail.com>
2018-10-06 22:46:30 +0200

    Add gh pages

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './js/components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter basename='/register-login-forms/'><App /></BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
